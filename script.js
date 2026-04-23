22bedfc34508d66d6b92a0464df12c45dced6580
Rupesh11 <rupeshgurung11@gmail.com>
2026-04-23 01:11:54 +0100

    Additional functions added

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const API_KEY = "CG-EXAgPfFigPe2qiSZuytxLjuc";
 
 const coinList = document.getElementById("coin-lists");
+const currencyButton = document.querySelector(".currency-button");
+let currency = localStorage.getItem("currency") ?? "usd";
 
+currencyButton.addEventListener("click", () => {
+  if (currency === "eur") {
+    currency = "usd";
+    renderCoins();
+  } else if (currency === "usd") {
+    currency = "eur";
+    renderCoins();
+  }
+  localStorage.setItem("currency", currency);
+});
 async function getCoinsData() {
-  const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&price_change_percentage=1h,24h,7d&sparkline=true&x_cg_demo_api_key=CG-EXAgPfFigPe2qiSZuytxLjuc";
+  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=10&page=1&price_change_percentage=1h,24h,7d&sparkline=true&x_cg_demo_api_key=${API_KEY}`;
 
   try {
     const response = await fetch(url);
@@ -17,9 +28,17 @@ async function getCoinsData() {
 
 async function renderCoins() {
   try {
-    loadingState();
+    if (coinList.children.length === 0) {
+      loadingState();
+    }
     const coinData = await getCoinsData();
+    let symbol = "$";
     let coinOutput = "";
+    if (currency === "usd") {
+      symbol = "$";
+    } else if (currency === "eur") {
+      symbol = "€";
+    }
 
     coinData.forEach((coin, index) => {
       const valueTwentyFourHr = coin.price_change_percentage_24h ?? 0;
@@ -32,24 +51,26 @@ async function renderCoins() {
 
       coinOutput += `
       <div class="coin-table">
-        <div class = "coin-name">
-        <div> ${index + 1} </div>
+      <div> ${index + 1} </div>
+
+      <div class = "coin-info">
         <img class="icon-picture" src="${coin.image}" alt="Coin Icon">
+        <div class = "coin-name">
         <h4>${coin.name}</h4>
         <p>${coin.symbol}</p>
-        </div>
-        <div class = "coin-stats">
-        <p>${coin.current_price}</p>
-        <p class="${oneHr.className}">${oneHr.sign}${valueOneHr.toFixed(1)}%</p>
-        <p class="${twentyFourHr.className}">${twentyFourHr.sign}${valueTwentyFourHr.toFixed(1)}%</p>
-        <p class="${sevenDays.className}">${sevenDays.sign}${valueSevenDays.toFixed(1)}%</p>
-        <p>${coin.total_volume}</p>
-        <p>${coin.market_cap}</p>
-        </div>
       </div>
+      </div>
+        <div>${symbol}${coin.current_price}</div>
+        <div class="${oneHr.className}">${oneHr.sign}${valueOneHr.toFixed(1)}%</div>
+        <div class="${twentyFourHr.className}">${twentyFourHr.sign}${valueTwentyFourHr.toFixed(1)}%</div>
+        <div class="${sevenDays.className}">${sevenDays.sign}${valueSevenDays.toFixed(1)}%</div>
+        <div>${symbol}${coin.total_volume}</div>
+        <div>${symbol}${coin.market_cap}</div>
+        <div>${symbol}${coin.market_cap}</div>
+        </div>
+
       `;
     });
-
     coinList.innerHTML = coinOutput;
   } catch (error) {
     console.log(error);
@@ -58,9 +79,16 @@ async function renderCoins() {
 }
 
 function autoRefresh() {
+  const countTimer = document.getElementById("counter");
+  let timeLeft = 60;
   setInterval(() => {
-    renderCoins();
-  }, 60000);
+    countTimer.textContent = `Page refresh in ${timeLeft}s `;
+    timeLeft--;
+    if (timeLeft <= 0) {
+      timeLeft = 60;
+      renderCoins();
+    }
+  }, 1000);
 }
 
 function loadingState() {
